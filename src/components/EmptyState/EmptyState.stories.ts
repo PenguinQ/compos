b5b42d3ed057dba3739fecf8ce27158ac21ec206
PenguinQ <pvpgns@yahoo.com>
2025-02-16 00:52:39 +0700

    docs: EmptyState docs and others

--- src/components/EmptyState/EmptyState.stories.ts
+++ src/components/EmptyState/EmptyState.stories.ts
@@ -1,11 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
+import type { ComponentProps } from 'vue-component-type-helpers';
 
-import { Button } from '@components';
+import { Button, Text } from '@components';
 import EmptyState from './EmptyState.vue';
 
-const meta: Meta<typeof EmptyState> = {
+type EmptyStateProps = ComponentProps<typeof EmptyState>;
+
+const meta: Meta<EmptyStateProps> = {
   component: EmptyState,
   argTypes: {
+    description: {
+      control: 'text',
+    },
+    emoji: {
+      control: 'text',
+    },
     image: {
       control: 'text',
     },
@@ -22,32 +31,39 @@ const meta: Meta<typeof EmptyState> = {
       control: 'select',
       options: ['vertical', 'horizontal'],
     },
+    subtitle: {
+      control: 'text',
+    },
+    title: {
+      control: 'text',
+    },
     width: {
       control: 'text',
     },
     height: {
       control: 'text',
     },
+    padding: {
+      control: 'text',
+    },
+    margin: {
+      control: 'text',
+    },
   },
   args: {
-    image: 'https://placehold.co/150x150',
+    image: 'https://upload-static.hoyoverse.com/hoyolab-wiki/2023/06/22/250281765/b6bbbd8e0c5c4bf49ead5a39b6429041_4585180775013259954.png',
     orientation: 'vertical',
-    title: 'Title text',
-    subtitle: 'Subtitle text',
-    description: 'Description text',
+    title: 'Aha',
+    subtitle: 'Aeon',
+    description: 'The Aeon of Elation. No one can predict what this Aeon might express THEIR mirth at. Joy is a right of sentient beings, and Aha inspires THEIR believers to delight in the joys of life. THEY take pleasure in the sharp turns of fate.',
   },
-  decorators: [
-    () => ({
-      template: '<div style="height: 400px; background-color: #FCF7F8;"><story /></div>',
-    }),
-  ],
 };
 
 export default meta;
 
-type EmptyState = StoryObj<typeof EmptyState>;
+type EmptyState = StoryObj<EmptyStateProps>;
 
-export const Default: EmptyState = {
+export const Playground: EmptyState = {
   render: (args) => ({
     components: { EmptyState },
     setup() {
@@ -59,17 +75,45 @@ export const Default: EmptyState = {
   }),
 };
 
-export const Slot: EmptyState = {
-  render: (args) => ({
-    components: { EmptyState, Button },
+export const DocUsage = {
+  tags: ['!dev'],
+  render: () => ({
+    components: { EmptyState },
     setup() {
-      return { args };
+      return {};
     },
     template: `
-      <EmptyState v-bind="args">
+      <EmptyState
+        image="https://upload-static.hoyoverse.com/hoyolab-wiki/2023/06/22/250281765/b6bbbd8e0c5c4bf49ead5a39b6429041_4585180775013259954.png"
+        title="Aha"
+        subtitle="Aeon"
+        description="The Aeon of Elation. No one can predict what this Aeon might express THEIR mirth at. Joy is a right of sentient beings, and Aha inspires THEIR believers to delight in the joys of life. THEY take pleasure in the sharp turns of fate."
+      />
+    `,
+  }),
+};
+
+export const DocSlots = {
+  tags: ['!dev'],
+  render: () => ({
+    components: { Button, EmptyState, Text },
+    setup() {
+      return {};
+    },
+    template: `
+      <EmptyState image="https://upload-static.hoyoverse.com/hoyolab-wiki/2023/06/22/250281765/b6bbbd8e0c5c4bf49ead5a39b6429041_4585180775013259954.png">
+        <template #title>
+          <Text heading="2" color="var(--color-red-4)" margin="0 0 4px">Aha?</Text>
+        </template>
+        <template #subtitle>
+          <Text heading="3" color="var(--color-blue-4)" margin="0 0 4px">? Aeon ?</Text>
+        </template>
+        <template #description>
+          <Text body="medium" color="var(--color-green-4)" margin="0">Aha is Aeon ??</Text>
+        </template>
         <template #action>
-          <Button>Button 1</Button>
-          <Button variant="outline">Button 2</Button>
+          <Button color="red" variant="outline">Fight me?</Button>
+          <Button>Fight me!</Button>
         </template>
       </EmptyState>
     `,
