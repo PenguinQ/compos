d1d7fb05f5dbbd2f3406f4138b1b912cba08af79
PenguinQ <pvpgns@yahoo.com>
2025-03-21 06:39:41 +0700

    feat: fix Toast unique key

--- src/components/Toast/hooks/useToast.ts
+++ src/components/Toast/hooks/useToast.ts
@@ -2,7 +2,9 @@ import { ref } from 'vue';
 import type { Ref } from 'vue';
 import type { ToastItem as ToastItemProps } from '../ToastItem.vue';
 
-type ToastItem = Omit<ToastItemProps, 'modelValue'>;
+type ToastItem = Omit<ToastItemProps, 'modelValue'> & {
+  id: number;
+};
 
 export type ToastReturn = {
   items: Ref<ToastItem[]>;
@@ -10,19 +12,21 @@ export type ToastReturn = {
   remove: () => void;
 };
 
+let toastId = 0;
+
 export default (): ToastReturn => {
-  const items = ref<ToastItem[]>([]);
+  const items   = ref<ToastItem[]>([]);
   const counter = ref(0);
 
   const add = (props: ToastItem) => {
-    items.value.push(props);
+    items.value.push({ ...props, id: ++toastId });
   };
 
   const remove = () => {
     counter.value += 1;
 
     if (items.value.length === counter.value) {
-      items.value = [];
+      items.value   = [];
       counter.value = 0;
     }
   };
